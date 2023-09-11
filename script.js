4d9d6f98e8672a3a1f46cc5447854a146bca8e2b
terra456 <terra.456@yandex.ru>
2023-09-11 15:02:37 +0500

    feat: add html and css

---
+++ script.js
@@ -0,0 +1,49 @@
+const languages = {
+  en: {
+    "nav__link--portfolio": "Portfolio",
+    "nav__link--projects": "Projects",
+    "nav__link--technologies": "Technologies",
+    "nav__link--about-me": "About me",
+    
+    "portfolio__head": "",
+    "portfolio__desc": "",
+    "portfolio__btn": "Resume for a job",
+
+    "projects__head": "Projects",
+    
+    "card__btn": "Link",
+    "card__head--1": "",
+    "card__desc--1": "",
+    
+    "technologies__head": "Technologies",
+    "about-me__head": "About me",
+
+    "contacts__head": "Call me:"
+
+  },
+
+  ru: {
+    "nav__link--portfolio": "Портфолио",
+    "nav__link--projects": "Проекты",
+    "nav__link--technologies": "Технологии",
+    "nav__link--about-me": "Обо мне",
+    
+    "portfolio__head": "",
+    "portfolio__desc": "",
+    "portfolio__btn": "Резюме",
+    
+    "projects__head": "Проекты",
+    
+    "card__btn": "Перейти",
+    "card__head--1": "",
+    "card__desc--1": "",
+    
+    "technologies__head": "Технологии",
+    "about-me__head": "Обо мне",
+
+    "contacts__head": "Телефон:"
+
+  }
+}
+
+console.log('hello');
